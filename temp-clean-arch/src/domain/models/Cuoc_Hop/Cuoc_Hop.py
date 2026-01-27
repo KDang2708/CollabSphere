9515777447c997c0bc6c4f84e38e35c122a4fcf1
Lợi <loihtn9278@ut.edu.vn>
2026-01-27 21:18:39 +0700

    Fix: remove embedded git repository

--- temp-clean-arch/src/domain/models/Cuoc_Hop/Cuoc_Hop.py
+++ temp-clean-arch/src/domain/models/Cuoc_Hop/Cuoc_Hop.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 class CuocHop:
-    def __init__(self, id: str, thoi_gian: str, noi_dung: str):
+    def __init__(self, id: str, thoi_gian: datetime, noi_dung: str):
         self.id = id
         self.thoi_gian = thoi_gian
         self.noi_dung = noi_dung
